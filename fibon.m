c7fe08d88c8de04819b0201edf18ef0bda8f9385
Soumyabrata Bhattacharjee <soumyabrata111@gmail.com>
2019-02-10 00:04:28 +0530

    Add files via upload
    This file finds out the desired Fibonacci Number. The function can be called as fibon(n) where n is the number for which Fibonacci Number is to be calculated. n.b. This file does not give Fibonacci Series or Sequence of numbers, rather only the deisred number.

---
+++ fibon.m
@@ -0,0 +1,14 @@
+function fibon(x)
+if x == 0
+    f_x = 0;
+    fprintf('The Fibonacci number is: %d\n',f_x)
+elseif x == 1
+    f_x = 1;
+    fprintf('The Fibonacci number is: %d\n',f_x)
+else
+    for i = 2:x
+        f_x = (x-1)+(x-2);
+    end
+    fprintf('The Fibonacci number is: %d\n',f_x)
+end
+
